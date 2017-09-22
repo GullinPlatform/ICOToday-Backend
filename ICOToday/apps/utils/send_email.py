7e2e3194898df9cb28182674e59bbfd21e3ba893
C0deZ Lee <c0dezlee@gmail.com>
2017-09-22 14:10:14 -0400

    Add utils

---
+++ ICOToday/apps/utils/send_email.py
@@ -0,0 +1,8 @@
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
+
+def send_email(receiver_list, subject, template_name, ctx):
+	email = EmailMessage(subject, render_to_string('email/%s.html' % template_name, ctx), 'no-reply@icotoday.io', receiver_list)
+	email.content_subtype = 'html'
+	email.send()

8bed868777b6230544efd6f22f694b56af44a1f8
C0deZ Lee <c0dezlee@gmail.com>
2017-11-03 17:12:46 +0000

    Update admin

--- ICOToday/apps/projects/admin.py
+++ ICOToday/apps/projects/admin.py
@@ -11,7 +11,7 @@ class ProjectAdmin(admin.ModelAdmin):
 	list_display = ('id', 'name', 'status', 'start_datetime', 'end_datetime')
 
 	fieldsets = (
-		('Relation Info', {'fields': ('company', 'status')}),
+		('Relation Info', {'fields': ('company', 'status', 'rating')}),
 		('Details', {'fields': ('name', 'logo_image', 'promote_image', 'category', 'description_short', 'description_full', 'token_sale_plan')}),
 		('ICO Details', {'fields': ('type', 'coin_name', 'initial_price', 'maximum_goal', 'minimum_goal', 'coin_unit', 'start_datetime', 'end_datetime', 'ratio', 'equality_on_offer', 'accept')}),
 		('Supplement', {'fields': ('website', 'video_link', 'white_paper')}),
